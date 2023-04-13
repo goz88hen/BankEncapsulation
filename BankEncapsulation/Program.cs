e8a49e7f8a20e257b013d1f34f8351a2ddd6b48a
Henry (Hendra) Gozali <123764602+goz88hen@users.noreply.github.com>
2023-04-12 22:14:36 -0700

    Encapsulation exercise has been completed.

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           var Henryaccount = new BankAccount { };
+            Console.WriteLine("How much woulld you like to deposit?");
+
+            double amountToDeposit = double.Parse(Console.ReadLine());
+
+            Henryaccount.Deposit(amountToDeposit);
+
+            Console.WriteLine($"Thank You! Your balance is now {Henryaccount.GetBalance()}");
+            //account.Balance = 300;
+
+            //Console.WriteLine(account.Balance);
+
         }
     }
 }
